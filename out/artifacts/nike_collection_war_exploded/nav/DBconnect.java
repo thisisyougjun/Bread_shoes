8ac72b07ae60c59a49415f4ec9fb1d237830f960
thisisyoungjun <122899@naver.com>
2021-10-10 12:27:09 +0900

    21-10-10

---
+++ out/artifacts/nike_collection_war_exploded/nav/DBconnect.java
@@ -0,0 +1,19 @@
+package portfolio.nav;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBconnect {
+    public static Connection getConnection(){
+        try{
+            String url = "jdbc:oracle:thin:@localhost:1521:XE";
+            String id = "system";
+            String pw = "1234";
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            return DriverManager.getConnection(url,id,pw);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
